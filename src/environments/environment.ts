5906b54f7f0fe8c34868eb4d7c5eeafe9bb582b8
Akhil <msakhil28@gmail.com>
2026-04-05 17:45:31 -0400

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     gemini: {
         // Encrypted Gemini API key (Base64 encoded)
-        apiKey: 'QUl6YVN5RFY1WmhTcnlTbXdLM3VsX0k4ZEc1dWRCUmhwY01BU2ZN'
+        apiKey: ''
     }
 };
